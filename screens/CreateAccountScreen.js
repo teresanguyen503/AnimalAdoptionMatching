709ec55a2c89a04a860be81d3eb5a8c361c5f133
Teresa Nguyen <nguyentup@gmail.com>
2024-04-10 14:40:14 -0700

    Created account screen, setting up form and validation

---
+++ screens/CreateAccountScreen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Formik } from 'formik'; 
+
+function CreateAccountScreen(props) {
+    return (
+        <View style={styles.container}></View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        
+    }
+})
+
+export default CreateAccountScreen;
