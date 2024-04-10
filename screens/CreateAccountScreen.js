38711a08843979966219f6c0612b2427f32f3e01
Teresa Nguyen <nguyentup@gmail.com>
2024-04-10 14:47:08 -0700

    Form set up for email and password, includes validation

--- screens/CreateAccountScreen.js
+++ screens/CreateAccountScreen.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { Formik } from 'formik'; 
+import * as Yup from 'yup'; 
+
+const validationSchema = Yup.object().shape({
+    email: Yup.string().required().email().label("Email"), 
+    password: Yup.string().required().min(6).label("Password")
+}); 
 
 function CreateAccountScreen(props) {
     return (
-        <View style={styles.container}></View>
+        <View style={styles.container}>
+            <Formik
+                initialValues={{email: '', password: ''}}
+                onSubmit={values => console.log(values)}
+                validationSchema={validationSchema}
+            >
+
+            </Formik>
+        </View>
     );
 }
 
