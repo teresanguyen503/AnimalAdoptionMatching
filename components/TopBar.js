5d1fec46c6fba8d740e0322d9d3902a328f5653b
seiunno <seiunno@gmail.com>
2024-04-17 21:32:51 -0700

    Trying to add Topbar

--- components/TopBar.js
+++ components/TopBar.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React from "react";
+import { View, Text } from "react-native";
+import SafeScreen from "./SafeScreen";
+import Icon from "react-native-vector-icons/AntDesign";
 
-const TopBar = ({ title }) => {
+const Topbar = ({ title, rightButton, leftButton }) => {
   return (
-    <View style={styles.topbar}>
-      <Text style={styles.title}>{title}</Text>
-    </View>
+    <SafeScreen>
+      <View style={styles.topbar}>
+        {leftButton && <TouchableOpacity>{leftButton}</TouchableOpacity>}
+        {iconName && <Icon name={menu} style={styles.icon} />}
+        <Text style={styles.title}>{title}</Text>
+        {rightButton && <TouchableOpacity>{rightButton}</TouchableOpacity>}
+        {iconName && <Icon name={back} style={styles.icon} />}
+      </View>
+    </SafeScreen>
   );
 };
 
 const styles = StyleSheet.create({
   topbar: {
     height: 50,
-    backgroundColor: '#f4f4f4',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#f4f4f4",
+    alignItems: "center",
+    justifyContent: "center",
     paddingHorizontal: 16,
   },
   title: {
-    fontSize: 18,
-    fontWeight: 'bold',
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+  icon: {
+    fontSize: 24,
+    marginRight: 10,
   },
 });
 
-export default TopBar;
+export default Topbar;
