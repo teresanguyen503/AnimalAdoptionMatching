18ae03467ad6acec918fbc65a48d6439e258ed15
Teresa Nguyen <nguyentup@gmail.com>
2024-04-23 08:04:13 -0700

    Corrected validation for charactor minimum on on password

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -8,7 +8,7 @@ import { AppFormField, SubmitButton } from '../components/forms';
 
 const validationSchema = Yup.object().shape({
     email: Yup.string().required().email().label("Email"), 
-    password: Yup.string().required().min(4).label("Password")
+    password: Yup.string().required().min(6).label("Password")
 }); 
 
 function LoginScreen(props) {
