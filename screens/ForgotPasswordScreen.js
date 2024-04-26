80cd5f7620803d231422b4b7165977eda3b6ea4d
Teresa Nguyen <nguyentup@gmail.com>
2024-04-26 13:23:17 -0700

    Implemented function to handle reset password

--- screens/ForgotPasswordScreen.js
+++ screens/ForgotPasswordScreen.js
@@ -5,19 +5,34 @@ import * as Yup from 'yup';
 
 import SafeScreen from '../components/SafeScreen';
 import { AppFormField, SubmitButton } from '../components/forms'; 
+import resetPassword from '../api/resetPassword'
 
 const validationSchema = Yup.object().shape({
     email: Yup.string().required().email().label("Email"), 
 }); 
 
 function ForgotPasswordScreen(props) {
+    const handleSubmit = async (values) => {
+        try {
+            const response = resetPassword.resetPasswordApi(values); 
+
+            if (!response.ok) {
+                alert('Failed to initiate password reset');
+            } else {
+                alert('Check your email to continue to reset your password.');
+            } 
+        } catch (error) {
+            alert('Something went wrong.'); 
+            console.log('Reset password error: ', error); 
+        }
+    }
     return (
         <SafeScreen>
             <View style={styles.container}>
                 <Text style={styles.text}>Enter the email that you want to reset the password for:</Text>
                 <Formik 
                     initialValues={{ email: '' }}
-                    onSubmit={values => console.log(values)}
+                    onSubmit={handleSubmit}
                     validationSchema={validationSchema}
                 >
                     { () => (
