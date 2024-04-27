8b5e38335045622c808ce8994661194f5573a52a
Teresa Nguyen <nguyentup@gmail.com>
2024-04-26 21:23:48 -0700

    Added logic to handle reset password

--- screens/ResetPasswordScreen.js
+++ screens/ResetPasswordScreen.js
@@ -10,18 +10,36 @@ const validationSchema = Yup.object().shape({
     password: Yup.string().required().min(6).label("Password")
 }); 
 
-function ResetPasswordScreen(props) {
+function ResetPasswordScreen({ navigation, route }) {
+    const { email } = route.params; 
+
+    const handleSubmit = (values) => {
+        try {
+            const response = await newPassword.newPasswordApi(values, email); 
+
+            if (!response.ok) {
+                alert('Something went wrong. Try again.'); 
+            } else {
+                alert('Great! Now log back in using your new password.');
+                navigation.navigate("Login"); 
+            } 
+        } catch (error) {
+            alert('Something went wrong.'); 
+            console.log('Reset password error: ', error); 
+        }
+    }
+
     return (
         <SafeScreen>
             <View style={styles.container}>
                 <Formik 
                     initialValues={{ password: '' }}
-                    onSubmit={values => console.log(values)}
+                    onSubmit={handleSubmit}
                     validationSchema={validationSchema}
                 >
                     { () => (
                         <>
-                            <Text style={styles.text}>Choose a new password.</Text>
+                            <Text style={styles.text}>Create a new password.</Text>
                             <AppFormField 
                                 autoCapitalize="none"
                                 autoCorrect={false}
