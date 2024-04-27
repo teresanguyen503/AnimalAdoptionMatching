8e2754fd9a2369e8de42e5ab59803b0dbfdc4a8c
Teresa Nguyen <nguyentup@gmail.com>
2024-04-27 11:57:03 -0700

    Put less spacing between pet containers

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -376,7 +376,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
   },
   petProfileContainer: {
-    width: "40%",
+    width: "49%",
     borderWidth: 1,
     borderColor: colors.black,
     borderRadius: 5,
