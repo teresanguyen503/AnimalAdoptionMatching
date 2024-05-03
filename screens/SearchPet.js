a9e2a89fe9fb6356b9afd79ba374fff0eaa050df
mmansoor05 <69745012+mmansoor05@users.noreply.github.com>
2024-05-03 15:36:33 -0400

    created breed dropdown

--- screens/SearchPet.js
+++ screens/SearchPet.js
@@ -2,15 +2,59 @@ import React, { useState } from 'react';
 import { FlatList, StyleSheet, Text, TextInput, TouchableOpacity, View, Modal } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
 import SafeScreen from '../components/SafeScreen';
+import dogData from '../breed-data/dog-breed.json';
+import catData from '../breed-data/cat-breed.json';
+import otherData from '../breed-data/other-breed.json';
 
 
 const SearchPet = () => {
     const [selectedButton, setSelectedButton] = useState(null);
+    const [selectedItem, setSelectedItem] = useState(null);
+    const [isOpen, setIsOpen] = useState(false);
+    const [searchQuery, setSearchQuery] = useState('');
+    const [value, setValue] = useState(null);
+    const [isFocus, setIsFocus] = useState(false);
 
     {/* Species Button Function */}
     const handleButtonPress = (buttonId) => {
         setSelectedButton(buttonId);
-}
+    }
+
+    // Filtered data based on search query
+const filteredDogData = dogData.filter(item =>
+    item.label.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+    {/* Select Breed Function */}
+    const filteredCatData = catData.filter(item =>
+    item.label.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+
+
+    const renderItem = ({ item }) => (
+      <TouchableOpacity onPress={() =>{handleItemPress(item); handleSelectedItem(item); }}>
+        <Text>{item.label}</Text>
+      </TouchableOpacity>
+    );
+    //console.log("selectedItem:", selectedItem)
+
+
+    const handleSelectedItem = (item) => {
+      setSelectedItem(item);
+      setIsOpen(false); // Close the dropdown after selecting an item
+    };
+    //console.log("selectedItem 2:", selectedItem.label)
+    let selectedData;
+    if (selectedButton === 1) {
+      selectedData = filteredDogData;
+        specieName = "Dog";
+    } else if (selectedButton === 2) {
+      selectedData = filteredCatData;
+       specieName = "Cat";
+    } else {
+      selectedData = otherData;
+        specieName = "Other";
+    }
+
 
 
     return(
@@ -47,8 +91,52 @@ const SearchPet = () => {
                         <Text style={styles.buttonText}>Other</Text>
                         </TouchableOpacity>
                     </View>
-
                 </View>
+
+                {/* Select Breed Header */}
+                <View>
+                    <Text style={styles.breedHeader}>Select Breed</Text>
+                    {/* Breed Dropdown */}
+                    <View>
+                        <TouchableOpacity onPress={() => setIsOpen(!isOpen)}>
+                            <View style={{ flexDirection: 'row', alignItems: 'center', borderWidth: 0.5, borderRadius: 5, borderColor: 'black', padding: 5, paddingLeft: 0, marginLeft: 12, marginRight: 25,}}>
+                                <Text style={{ paddingLeft: 22}}>{selectedItem ? selectedItem.label : 'Select Breed'}</Text>
+                                <MaterialIcons name={isOpen ? 'arrow-drop-up' : 'arrow-drop-down'}  size={24} />
+                            </View>
+                        </TouchableOpacity>
+                        {isOpen && (
+                        <View style={[styles.dropdown, isFocus && { borderColor: 'blue' }]}>
+                            <TextInput
+                            placeholder="Search..."
+                            onChangeText={setSearchQuery}
+                            value={searchQuery}
+                            />
+                            <FlatList
+                            data={selectedData}
+                            placeholderStyle={styles.placeholderStyle}
+                            selectedTextStyle={styles.selectedTextStyle}
+                            inputSearchStyle={styles.inputSearchStyle}
+                            iconStyle={styles.iconStyle}
+                            search
+                            labelField="label"
+                            valueField="value"
+                            placeholder={!isFocus ? 'Select Breed' : '...'}
+                            searchPlaceholder="Search..."
+                            value={value}
+                            onFocus={() => setIsFocus(true)}
+                            onBlur={() => setIsFocus(false)}
+                            onChange={item => {
+                            setValue(item);
+                            setIsFocus(false);
+                            }}
+                            renderItem={renderItem}
+                            keyExtractor={(item) => item.value.toString()}
+                            />
+                        </View>
+                        )}
+                    </View>
+                </View>
+
             </View>
         </SafeScreen>
     )
@@ -110,7 +198,36 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         alignItems: 'center',
         padding: 16,
-
+    },
+    /* Dropdown Styling */
+    dropdown: {
+        // height: 50,
+        borderColor: 'gray',
+        borderWidth: 0.5,
+        borderRadius: 8,
+        paddingHorizontal: 8,
+        marginLeft: 12,
+        marginRight: 24,
+    },
+    placeholderStyle: {
+        fontSize: 16,
+    },
+    selectedTextStyle: {
+        fontSize: 16,
+    },
+    iconStyle: {
+        width: 20,
+        height: 20,
+    },
+    inputSearchStyle: {
+        height: 40,
+        fontSize: 16,
+    },
+    breedHeader: {
+        paddingLeft: 22,
+        fontSize: 18,
+        fontWeight: 'bold',
+        marginTop: -2,
     },
 
 })
