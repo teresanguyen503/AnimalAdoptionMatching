74740aa29a53f4eda56730526b6d8e69a0d96fda
mmansoor05 <69745012+mmansoor05@users.noreply.github.com>
2024-05-04 07:21:08 -0400

    changed FlatList to ScrollView for easy viewing all filtered profiles

--- components/FilterPet.js
+++ components/FilterPet.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, View, Text, Image, FlatList} from 'react-native';
+import { StyleSheet, View, Text, Image, FlatList, ScrollView} from 'react-native';
 import axios from 'axios';
 
 
@@ -7,35 +7,39 @@ export default function FilterPet({onFilteredProfiles}){
 
     return (
         <View>
-            <FlatList
-                data={onFilteredProfiles}
-                renderItem={({ item }) => (
-                    <View style={{ flexDirection: 'row', alignItems: 'center', padding: 10 }}>
-                        <Image
-                            source={{ uri: item.image }}
-                            style={{ width: 50, height: 50, borderRadius: 25, marginRight: 10 }}
-                        />
-                        <View>
-                            <Text style={{ fontWeight: 'bold' }}>{item.name}</Text>
-                            {item.isdate !== undefined && (
-                            <Text style={styles.profileData}>Date Available: {new Date(item.isdate).toDateString()}</Text>
-                            )}
-                            <Text>Description: {item.desc}</Text>
-                            <Text>Animal Type: {item.speciesName}</Text>
-                            <Text>Breed: {item.selectedItem.label}</Text>
-                            <Text>Disposition: {item.disposition}</Text>
-                            {/* Add more profile details as needed */}
-                        </View>
-                    </View>
-                )}
-                keyExtractor={item => item._id}
-            />
+            <View style={styles.filteredProfileContainer}>
+                <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+                  {onFilteredProfiles.map(profile => (
+                 <View key={profile._id} style={{  flexDirection: 'row', alignItems: 'center', padding: 10 }}>
+                 <View style={{ flexDirection: 'row', alignItems: 'center', padding: 10 }}>
+                 <Image
+                     source={{ uri: profile.image }}
+                     style={{ width: 50, height: 50, borderRadius: 25, marginRight: 10 }}
+                 />
+                 </View>
+                 <View>
+                     <Text style={{ fontWeight: 'bold' }}>{profile.name}</Text>
+                     {profile.isdate !== undefined && (
+                     <Text style={styles.profileData}>Date Available: {new Date(profile.isdate).toDateString()}</Text>
+                     )}
+                     <Text>Description: {profile.desc}</Text>
+                     <Text>Animal Type: {profile.speciesName}</Text>
+                     <Text>Breed: {profile.selectedItem.label}</Text>
+                     <Text>Disposition: {profile.disposition}</Text>
+                 </View>
 
-
+            </View>
+            ))}
+                </ScrollView>
+              </View>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
+    filteredProfileContainer: {
+        width: "100%",
+        maxHeight: 350,
+    },
 
 })
